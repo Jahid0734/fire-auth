9c5ec5e404019ec776aec3da59cc7ab707afb3a8
unknown <Jahid0734@gmail.com>
2020-03-20 15:14:32 +0600

    fire-auth

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDZTwHMJ0G2XHFroLWc1zJaxy2SnmDsNvA",
+    authDomain: "chhaya-baz.firebaseapp.com",
+    databaseURL: "https://chhaya-baz.firebaseio.com",
+    projectId: "chhaya-baz",
+    storageBucket: "chhaya-baz.appspot.com",
+    messagingSenderId: "839139895441",
+    appId: "1:839139895441:web:1fc4adaa5bce35ce5db730"
+  };
+  
+  export default firebaseConfig;
